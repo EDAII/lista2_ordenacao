81f04f0f679a2aa92459278cba4b3d8654feb439
victorhdcoelho <victorhdcoelho@gmail.com>
2018-03-30 15:59:34 -0300

    Insertion sort all graph

--- main.py
+++ main.py
@@ -5,95 +5,181 @@ from Vector import Vector
 from Sort import Sort
 import time
 import timeit
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    vector = Vector.vector_unsorted(100)
+    ''' Graph of insertion sort in time '''
 
-    #Insertion sort --------------------------------------------
-    vector = Vector.vector_unsorted(100)
-    print('| Insertion sort com vetor desordenado de tamanho 100 |')
-    tic = timeit.default_timer()
-    nums1 = Sort.insertion_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
-    
-    vector = Vector.vector_unsorted(1000)
-    print('| Insertion sort com vetor desordenado de tamanho 1000 |')
-    tic = timeit.default_timer()
-    nums2 = Sort.insertion_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
-    
-    vector = Vector.vector_unsorted(10000)
-    print('| Insertion sort com vetor desordenado de tamanho 10000 |')
-    tic = timeit.default_timer()
-    nums2 = Sort.insertion_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    ''' Unsorted Vector '''
+    list_result = []
+    list_index = []
+    for count in range(1,5000,50):
+        vector = Vector.vector_unsorted(count)
+        list_index.append(count)
+        tic = timeit.default_timer()
+        vector_sorted = Sort.insertion_sort(vector)
+        toc = timeit.default_timer()
+        time = toc - tic
+        list_result.append(time)
+    #print("list_index : ",list_index)
+    #print("list_result: ",list_result)
+    f, axarr = plt.subplots(5, sharex=True)
+    axarr[0].plot(list_index, list_result)
+    axarr[0].set_title('Insertion sort unsorted vector')
+    ''' Sorted Vector '''
+    list_result = []
+    list_index = []
+    for count in range(1,5000,100):
+        vector = Vector.vector_sorted(count)
+        list_index.append(count)
+        tic = timeit.default_timer()
+        vector_sorted = Sort.insertion_sort(vector)
+        toc = timeit.default_timer()
+        time = toc - tic
+        list_result.append(time)
+    #print("list_index : ",list_index)
+    #print("list_result: ",list_result)
+    axarr[1].plot(list_index,list_result)
+    axarr[1].set_title("Insertion sort in sorted vector")
+    ''' Semi unsorted Vector '''
+    list_result = []
+    list_index = []
+    for count in range(1,5000,100):
+        vector = Vector.vector_semi_unsorted(count)
+        list_index.append(count)
+        tic = timeit.default_timer()
+        vector_sorted = Sort.insertion_sort(vector)
+        toc = timeit.default_timer()
+        time = toc - tic
+        list_result.append(time)
+    #print("list_index : ",list_index)
+    #print("list_result: ",list_result)
+    axarr[2].plot(list_index,list_result)
+    axarr[2].set_title("Insertion sort in semi unsorted vector")
 
-    #Shell sort --------------------------------------------
-    vector = Vector.vector_unsorted(100)
-    print('| Shell sort com vetor desordenado de tamanho 100 |')
-    tic = timeit.default_timer()
-    nums1 = Sort.shell_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
-    
-    vector = Vector.vector_unsorted(1000)
-    print('| Shell sort com vetor desordenado de tamanho 1000 |')
-    tic = timeit.default_timer()
-    nums2 = Sort.shell_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
-    
-    vector = Vector.vector_unsorted(10000)
-    print('| Shell sort com vetor desordenado de tamanho 10000 |')
-    tic = timeit.default_timer()
-    nums2 = Sort.shell_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    ''' Decreasing Vector '''
+    list_result = []
+    list_index = []
+    for count in range(1,5000,100):
+        vector = Vector.vector_decreasing(count)
+        list_index.append(count)
+        tic = timeit.default_timer()
+        vector_sorted = Sort.insertion_sort(vector)
+        toc = timeit.default_timer()
+        time = toc - tic
+        list_result.append(time)
+    #print("list_index : ",list_index)
+    #print("list_result: ",list_result)
+    axarr[3].plot(list_index,list_result)
+    axarr[3].set_title("Insertion sort in decreasing vector")
+    ''' Repeater Vector '''
+    list_result = []
+    list_index = []
+    for count in range(1,5000,100):
+        vector = Vector.vector_repeater(count)
+        list_index.append(count)
+        tic = timeit.default_timer()
+        vector_sorted = Sort.insertion_sort(vector)
+        toc = timeit.default_timer()
+        time = toc - tic
+        list_result.append(time)
+    #print("list_index : ",list_index)
+    #print("list_result: ",list_result)
+    axarr[4].plot(list_index,list_result)
+    axarr[4].set_title("Insertion sort in repeater vector")
 
-    #Selection sort --------------------------------------------
-    vector = Vector.vector_unsorted(100)
-    print('| Selection sort com vetor desordenado de tamanho 100 |')
-    tic = timeit.default_timer()
-    nums1 = Sort.selection_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
-    
-    vector = Vector.vector_unsorted(1000)
-    print('| Selection sort com vetor desordenado de tamanho 1000 |')
-    tic = timeit.default_timer()
-    nums2 = Sort.selection_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
-    
-    vector = Vector.vector_unsorted(10000)
-    print('| Selection sort com vetor desordenado de tamanho 10000 |')
-    tic = timeit.default_timer()
-    nums2 = Sort.selection_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
 
-    #Bubble sort --------------------------------------------
-    vector = Vector.vector_unsorted(100)
-    print('| Bubble sort com vetor desordenado de tamanho 100 |')
-    tic = timeit.default_timer()
-    nums1 = Sort.bubble_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
-    
-    vector = Vector.vector_unsorted(1000)
-    print('| Bubble sort com vetor desordenado de tamanho 1000 |')
-    tic = timeit.default_timer()
-    nums2 = Sort.bubble_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
-    
-    vector = Vector.vector_unsorted(10000)
-    print('| Bubble sort com vetor desordenado de tamanho 10000 |')
-    tic = timeit.default_timer()
-    nums2 = Sort.bubble_sort(vector)
-    toc = timeit.default_timer()
-    print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    f.subplots_adjust(hspace=0.6,right= 0.5, left= 0.2,top=1.2)
+    plt.show()
+
+
+    # vector = Vector.vector_unsorted(100)
+    #
+    # #Insertion sort --------------------------------------------
+    # vector = Vector.vector_unsorted(100)
+    # print('| Insertion sort com vetor desordenado de tamanho 100 |')
+    # tic = timeit.default_timer()
+    # nums1 = Sort.insertion_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # vector = Vector.vector_unsorted(1000)
+    # print('| Insertion sort com vetor desordenado de tamanho 1000 |')
+    # tic = timeit.default_timer()
+    # nums2 = Sort.insertion_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # vector = Vector.vector_unsorted(10000)
+    # print('| Insertion sort com vetor desordenado de tamanho 10000 |')
+    # tic = timeit.default_timer()
+    # nums2 = Sort.insertion_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # #Shell sort --------------------------------------------
+    # vector = Vector.vector_unsorted(100)
+    # print('| Shell sort com vetor desordenado de tamanho 100 |')
+    # tic = timeit.default_timer()
+    # nums1 = Sort.shell_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # vector = Vector.vector_unsorted(1000)
+    # print('| Shell sort com vetor desordenado de tamanho 1000 |')
+    # tic = timeit.default_timer()
+    # nums2 = Sort.shell_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # vector = Vector.vector_unsorted(10000)
+    # print('| Shell sort com vetor desordenado de tamanho 10000 |')
+    # tic = timeit.default_timer()
+    # nums2 = Sort.shell_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # #Selection sort --------------------------------------------
+    # vector = Vector.vector_unsorted(100)
+    # print('| Selection sort com vetor desordenado de tamanho 100 |')
+    # tic = timeit.default_timer()
+    # nums1 = Sort.selection_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # vector = Vector.vector_unsorted(1000)
+    # print('| Selection sort com vetor desordenado de tamanho 1000 |')
+    # tic = timeit.default_timer()
+    # nums2 = Sort.selection_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # vector = Vector.vector_unsorted(10000)
+    # print('| Selection sort com vetor desordenado de tamanho 10000 |')
+    # tic = timeit.default_timer()
+    # nums2 = Sort.selection_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # #Bubble sort --------------------------------------------
+    # vector = Vector.vector_unsorted(100)
+    # print('| Bubble sort com vetor desordenado de tamanho 100 |')
+    # tic = timeit.default_timer()
+    # nums1 = Sort.bubble_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # vector = Vector.vector_unsorted(1000)
+    # print('| Bubble sort com vetor desordenado de tamanho 1000 |')
+    # tic = timeit.default_timer()
+    # nums2 = Sort.bubble_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
+    #
+    # vector = Vector.vector_unsorted(10000)
+    # print('| Bubble sort com vetor desordenado de tamanho 10000 |')
+    # tic = timeit.default_timer()
+    # nums2 = Sort.bubble_sort(vector)
+    # toc = timeit.default_timer()
+    # print("| Tempo de execução: {} |\n\n".format(toc-tic))
